19465139e4b4b9fab7e1beed72fd96c043b4afa6
derrick <derrick.mwangi2@student.moringaschool.com>
2025-06-07 00:00:51 +0300

    Complete Scuber scope lab: declare variables and write functions to manipulate them

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// 1. Declare customerName globally with var and assign 'bob'
+var customerName = 'bob';
+
+// 2. Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function that declares bestCustomer in global scope and assigns 'not bob'
+function setBestCustomer() {
+  // Declaring bestCustomer without var, let, or const makes it global
+  bestCustomer = 'not bob';
+}
+
+// 4. Function to overwrite bestCustomer global variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Declare leastFavoriteCustomer as a const in global scope
+const leastFavoriteCustomer = 'some customer';
+
+// 6. Function to attempt to change leastFavoriteCustomer constant
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because constants cannot be reassigned
+  leastFavoriteCustomer = 'another customer'; 
+}
